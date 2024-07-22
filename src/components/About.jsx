3758be95ba77986e7d73615cbb1cf28002901a54
Jonathan Hooker <Jonathan@oblio.io>
2024-07-22 15:24:20 -0700

    about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,9 +9,9 @@ const About = ({ }) => {
       <div className='bodyCopy'>Over the last 20 years I've had the opportunity to build websites, games, mobile and desktop apps, AR filters, and photobooths for some amazing clients.</div>
       <div className='bodyCopy'>I see learning as a life long journey. I'm constantly experimenting with new applications and coding languages. The harder a challenge is, the more it excites me.</div> */}
       <div className='bodyCopy'>
-        <p>That can be hard to answer succintly since I wear a lot of hats, but the short answer is that I'm a creative engineer: I love to code and I love using that skill to make cool things.</p>
-        <p>I started my career as a visual designer but quickly found that my tech side took over. Over the last 20 years I've had the opportunity to build websites, games, mobile and desktop apps, AR filters, and photobooths for some amazing clients.</p>
-        <p>I see learning as a life long journey. I'm constantly experimenting with new applications and coding languages. The harder a challenge is, the more it excites me.</p>
+        <p>That can be hard to answer succintly since I wear a lot of hats, but the short answer is that I&apos;m a creative engineer: I love to code and I love using that skill to make cool things.</p>
+        <p>I started my career as a visual designer but quickly found that my tech side took over. Over the last 20 years I&apos;ve had the opportunity to build websites, games, mobile and desktop apps, AR filters, and photobooths for some amazing clients.</p>
+        <p>I see learning as a life long journey. I&apos;m constantly experimenting with new applications and coding languages. The harder a challenge is, the more it excites me.</p>
       </div>
       {/* <div className="spacer"></div> */}
       <div className="spacer"></div>
